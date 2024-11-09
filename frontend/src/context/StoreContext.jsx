bdf4ee7b58ba171194377afeb3eacb03e309efb1
Karthick Raja <karthickrajab03@gmail.com>
2024-11-09 11:30:35 +0530

    Small Error Fixed

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-    import { createContext, useState } from "react";
+    import { createContext, useState, useEffect } from "react";
     import { food_list } from "../assets/assets";
 
     export const StoreContext = createContext(null) 
