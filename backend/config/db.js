0ec4678113f5f58dcfb4c4a501cc573a35803c35
Karthick Raja <karthickrajab03@gmail.com>
2025-06-01 11:01:39 +0530

    final correction

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://karthickrajab03:Vidyalaya005@cluster0.j7wfu.mongodb.net/food-del').then(()=>console.log("DBconnect"));
+    try {
+        await mongoose.connect(process.env.MONGODB_API);
+        console.log("DBconnect");
+    } catch (error) {
+        console.error("Database connection error:", error);
+        process.exit(1);
+    }
 }
--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -5,7 +5,7 @@ const header = () => {
   return (
     <div className='header'>
       <div className="header-content">
-        <h2>Oder your favorite food from here</h2>
+        <h2>Order your favorite food from here</h2>
         <p>Choose from the diverse menu featuring a delectable array of dishes crafted with the finest ingrediate to satisfy your cravings and your dining experience, one delicious meal at a time.</p>
         <button>view Menu</button>
       </div>
